275f4ef101c636304bae3ebb8cc96f445f3d58e7
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-07-22 12:18:18 -0500

    adding ads seeder and adsdao walkthrough code

--- src/main/java/models/MySQLAdsDao.java
+++ src/main/java/models/MySQLAdsDao.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public class MySQLAdsDao implements Ads {
 
-    private Connection connection = null;
+    private Connection connection;
 
     public MySQLAdsDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
-            connection = DriverManager.getConnection(
+            this.connection = DriverManager.getConnection(
                     config.getUrl(),
                     config.getUser(),
                     config.getPassword());
@@ -23,53 +23,107 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public List<Ad> all() {
-        Statement stmt = null;
-        try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
-            return createAdsFromResults(rs);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public Long insert(Ad ad) {
-        try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String createInsertQuery(Ad ad) {
-        return "INSERT INTO ads(user_id, title, description) VALUES "
-                + "(" + ad.getUserId() + ", "
-                + "'" + ad.getTitle() +"', "
-                + "'" + ad.getDescription() + "')";
-    }
-
-    private Ad extractAd(ResultSet rs) throws SQLException {
-        return new Ad(
-                rs.getLong("id"),
-                rs.getLong("user_id"),
-                rs.getString("title"),
-                rs.getString("description")
-        );
-    }
-
-    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+        //Array list to store ads into
         List<Ad> ads = new ArrayList<>();
-        while (rs.next()) {
-            ads.add(extractAd(rs));
+        //Query that will get all ads from ads table in adlister_db
+        String selectAds = "SELECT * FROM ads";
+        try {
+            //creating statement instance
+            Statement stmt = connection.createStatement();
+            //executing statement and storing in result set
+            ResultSet rs = stmt.executeQuery(selectAds);
+            //while rs.next is true (returns true until there are now more rows to go through) a new ad will be created
+            while (rs.next()) {
+                ads.add(new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description"))
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return ads;
     }
+
+//    @Override
+//    public List<Ad> all() {
+//        //Array list to store ads into
+//        List<Ad> allAdAdsList = new ArrayList<>();
+//        //Query that will get all ads from ads table in adlister_db
+//        String getAllQuery = "SELECT * FROM ads";
+//        try {
+//            //creating statement instance
+//            Statement statement = connection.createStatement();
+//            //executing statement and storing in result set
+//            ResultSet rs = statement.executeQuery(getAllQuery);
+//            //while rs.next is true (returns true until there are now more rows to go through) a new ad will be created
+//            while (rs.next()) {
+//                allAdAdsList.add(new Ad(
+//                        rs.getLong("id"),
+//                        rs.getLong("user_id"),
+//                        rs.getString("title"),
+//                        rs.getString("description"))
+//                );
+//            }
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        return allAdAdsList;
+//    }
+
+    @Override
+    public Long insert(Ad ad) {
+        long lastInsertedAdId = 0;
+        String insertQuery = String.format("INSERT INTO ads (user_id, title, description) VALUES (%d,'%s','%s')",
+                ad.getUserId(),
+                ad.getTitle(),
+                ad.getDescription()
+        );
+
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                lastInsertedAdId = rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lastInsertedAdId;
+    }
+
+
+//    @Override
+//    public Long insert(Ad ad) {
+//        long insertedAdId = 0;
+//        String insertQuery = String.format("INSERT INTO ads (user_id, title, description) VALUES ('%d','%s','%s')",
+//                ad.getUserId(),
+//                ad.getTitle(),
+//                ad.getDescription()
+//        );
+//
+//        try {
+//            Statement statement = connection.createStatement();
+//            statement.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+//            ResultSet rsGenKeys = statement.getGeneratedKeys();
+//            if (rsGenKeys.next()) {
+//                insertedAdId = rsGenKeys.getLong(1);
+//            }
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        return insertedAdId;
+//    }
+
+    public static void main(String[] args){
+            MySQLAdsDao adsDao = new MySQLAdsDao(new Config());
+//        System.out.println(adsDao.all());
+        Ad testAd = new Ad(1, "test ad title", "test ad description");
+        System.out.println(adsDao.insert(testAd)); //shows a new add number 6
+    }
+
 }
 
